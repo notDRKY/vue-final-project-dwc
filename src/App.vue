7cc1fcb7b39c6c6a593ae4f5861ed71725e195b3
notDRKY <darkylyon@gmail.com>
2023-10-22 06:24:04 +0200

    form styles changed

--- src/App.vue
+++ src/App.vue
@@ -21,17 +21,132 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <AppMenu></AppMenu>
+  <AppMenu></AppMenu>
+  <div id="container">
     <ProductsTable></ProductsTable>
     <ProductForm></ProductForm>
-    <footer><small>Diseño Web en Entorno Cliente - IES Mestre Ramon Esteve</small></footer>
   </div>
+  <footer><small>Diseño Web en Entorno Cliente - IES Mestre Ramon Esteve</small></footer>
 </template>
 
 <style>
-  body {
-    font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
-    font-size: 18px;
-  }
+body {
+  color: #f2f2f2;
+  background-color: #2f2f2f;
+  font-family: ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  font-size: 18px;
+}
+
+#container {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+}
+
+table {
+  flex: 1;
+  width:100%;
+}
+
+form {
+  flex: 1;
+  width: 100%;
+  box-sizing: border-box;
+  align-items: center;
+}
+
+form .form_cont {
+  padding:1rem;
+}
+
+form .input-group {
+  position: relative;
+  margin: 20px 0;
+}
+
+form .input-group label {
+  position: absolute;
+  top: 50%;
+  left: 5px;
+  transform: translateY(-50%);
+  font-size: 18px;
+  color: #f2f2f2;
+  padding: 0 5px;
+  pointer-events: none;
+  transition: .5s;
+}
+
+form .input-group input {
+  margin: 0;
+  padding: 0;
+  font-size: 18px;
+  height: 2rem;
+  min-width: 100%;
+  color: #f2f2f2;
+  background-color: transparent;
+  border: 1.25px solid #f2f2f2;
+  border-radius: 12px;
+  outline: none;
+}
+
+form .input-group input:focus~label,
+form .input-group input:valid~label {
+  top: 0;
+  font-size: 14px;
+  background: #2f2f2f;
+}
+
+form select {
+  margin: 0;
+  padding: 0;
+  font-size: 18px;
+  height: 2rem;
+  min-width: 100%;
+  text-align: center;
+  color: #f2f2f2;
+  background-color: transparent;
+  border: 1.25px solid #f2f2f2;
+  border-radius: 12px;
+}
+
+form select option {
+  outline: none;
+  background-color: #2f2f2f;
+}
+
+form .buttons {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+form button {
+  margin: 1rem 0.125rem 0.125rem 0.125rem;
+  color: #f2f2f2;
+  font-size: 18px;
+  min-height: 2rem;
+  border-radius: 10px;
+  border: 0.1rem transparent;
+  cursor: pointer;
+  background-color: #ff3d43;
+  flex: 1;
+}
+
+form button:hover {
+  background-color: #b22a2e;
+  transition: .5s;
+}
+
+form button[type=submit] {
+  color: #f2f2f2;
+  background-color: #198754;
+  flex: 1;
+}
+
+form button[type=submit]:hover {
+  background-color: #0f5434;
+  transition: .5s;
+}
+
 </style>
--- src/components/ProductForm.vue
+++ src/components/ProductForm.vue
@@ -1,47 +1,42 @@
 <template>
-	<div class="row">
-		<div class="col-sm-12 col-md-8 col-lg-8">
-			<form @submit.prevent="submitForm">
-				<fieldset>
-					<legend class="bg-dark text-white text-center">Añadir producto</legend>
-					<!-- Aquí los inputs y botones del form -->
-					<div class="form-group">
-						<label for="newprod-id">ID:</label>
-						<input type="text" v-model="product.id" class="form-control" disabled>
-					</div>
-					<div class="form-group">
-						<label for="newprod-name">Nombre:</label>
-						<input type="text" v-model="product.name" class="form-control" required>
-						<span class="error"></span>
-					</div>
+	<form @submit.prevent="submitForm">
+		<div class="form_cont">
+			<h1>Añadir producto</h1>
+			<!-- Aquí los inputs y botones del form -->
 
-					<div class="form-group">
-						<label for="newprod-cat">Categoría:</label>
-						<select v-model="product.category" class="form-control" required>
-							<option value="">--- Selecciona categoría ---</option>
-							<option v-for="cat in categories" :key="cat.id" :value="cat.id" :title="cat.description">{{ cat.name }}
-							</option>
-						</select>
-						<span class="error"></span>
-					</div>
+			<div class="input-group">
+				<input type="text" v-model="product.id" disabled>
+				<label for="newprod-id">ID</label>
+			</div>
 
-					<div class="form-group">
-						<label for="newprod-units">Unidades.:</label>
-						<input type="number" v-model="product.units" class="form-control" min="0" step="1">
-						<span class="error"></span>
-					</div>
-					<div class="form-group">
-						<label for="newprod-price">Precio/u.:</label>
-						<input type="number" v-model="product.price" class="form-control" required min="0" step="0.01">
-						<span class="error"></span>
-					</div>
-					<br>
-					<button type="submit" class="btn btn-default btn-dark">Añadir</button>
-					<button type="reset" class="btn btn-danger">Reset</button>
-				</fieldset>
-			</form>
+			<div class="input-group">
+				<input type="text" v-model="product.name" required>
+				<label for="newprod-name">Nombre</label>
+			</div>
+
+			<div class="input-group">
+				<input type="number" v-model="product.units" required min="0" step="1">
+				<label for="newprod-units">Unidades</label>
+			</div>
+
+			<div class="input-group">
+				<input type="number" v-model="product.price" required min="0" step="0.01">
+				<label for="newprod-price">Precio</label>
+			</div>
+			<div>
+				<label for="newprod-cat"></label>
+				<select v-model="product.category" required>
+					<option value="">--- Selecciona categoría ---</option>
+					<option v-for="cat in categories" :key="cat.id" :value="cat.id" :title="cat.description">{{ cat.name }}
+					</option>
+				</select>
+			</div>
+			<div class="buttons">
+				<button type="submit">Añadir</button>
+				<button type="reset">Reset</button>
+			</div>
 		</div>
-	</div>
+	</form>
 </template>
 
 <script>
