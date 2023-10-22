39ffdbc6b085e845fc158d9b54bbcae98d348476
notDRKY <darkylyon@gmail.com>
2023-10-22 04:21:48 +0200

    removed bootstrap classes

--- src/components/ProductsTable.vue
+++ src/components/ProductsTable.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
     <h1>Listado de productos</h1>
-    <table v-if="products.length" class="table table-striped">
-      <thead class="table-dark">
+    <table v-if="products.length">
+      <thead>
         <tr>
-          <td class="text-center">Id</td>
-          <td class="text-center">Nombre</td>
-          <td class="text-center">Categoría</td>
-          <td class="text-center">Uds.</td>
-          <td class="text-center">Precio/u</td>
-          <td class="text-center">Importe</td>
-          <td class="text-center">Acciones</td>
+          <td>Id</td>
+          <td>Nombre</td>
+          <td>Categoría</td>
+          <td>Uds.</td>
+          <td>Precio/u</td>
+          <td>Importe</td>
+          <td>Acciones</td>
         </tr>
       </thead>
       <tbody>
@@ -53,3 +53,10 @@ export default {
   }
 }
 </script>
+
+<style>
+  thead {
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+</style>
